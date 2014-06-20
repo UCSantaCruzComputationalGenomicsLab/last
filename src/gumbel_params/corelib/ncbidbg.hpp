40353a77f1bb72a45598cb1ef74a4ff08c71630b
benedictpaten <benedictpaten@gmail.com>
2014-06-20 13:22:03 -0700

    Updating version.

--- src/gumbel_params/corelib/ncbidbg.hpp
+++
@@ -1,8 +0,0 @@
-#ifndef CORELIB___NCBIDBG__HPP
-#define CORELIB___NCBIDBG__HPP
-
-#  define NCBI_ASSERT(expr, mess)   ((void)0)
-
-#define _ASSERT(expr)   NCBI_ASSERT(expr, NULL)
-
-#endif  /* CORELIB___NCBIDBG__HPP */
--- src/gumbel_params/corelib/ncbitype.h
+++
@@ -1,7 +0,0 @@
-#ifndef CORELIB___NCBITYPE__H
-#define CORELIB___NCBITYPE__H
-
-typedef signed   int   Int4;    /**< Alias for signed int */
-typedef unsigned int   Uint4;   /**< Alias for unsigned int */
-
-#endif  /* CORELIB___NCBITYPE__H */
